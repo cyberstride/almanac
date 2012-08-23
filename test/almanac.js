d30ef977c28dca39e3bf5976a1a483ce563431bc
Chance Dinkins <chance@cyberstride.net>
2012-08-23 01:04:33 -0400

    initial test cases

---
+++ test/almanac.js
@@ -0,0 +1,97 @@
+describe('almanac', function(){
+  
+
+  describe('single', function(){
+    before(function(){
+    });
+    it('should match a range', function(){
+    });
+    it('next should be undefined', function(){
+    }
+  });
+  describe('daily', function(){
+    before(function(){
+    });
+
+    describe('parsing', function(){
+      it('should have the correct interval', function(){
+      });
+    });
+    describe('matches', function(){
+      it('should match a moment that overlaps', function(){
+      });
+      it('should match a moment range that overlaps', function(){
+      });
+      it('should not match a moment that does not overlap', function(){
+      });
+      it('should not match a range that does not overlap', function(){
+      });
+    });
+  });
+
+
+  describe('weekly', function(){
+    before(function(){
+    });
+
+    describe('parsing', function(){
+      it('should have the correct interval', function(){
+      });
+    });
+    describe('matches', function(){
+      it('should have the correct interval', function(){
+      });
+      it('should match a moment that overlaps', function(){
+      });
+      it('should match a moment range that overlaps', function(){
+      });
+      it('should not match a moment that does not overlap', function(){
+      });
+      it('should not match a range that does not overlap', function(){
+      });
+    });
+  });
+
+  describe('daymonthly', function(){
+    before(function(){
+    });
+
+    describe('parsing', function(){
+      it('should have the correct interval', function(){
+      });
+    });
+    describe('matches', function(){
+      it('should have the correct interval', function(){
+      });
+      it('should match a moment that overlaps', function(){
+      });
+      it('should match a moment range that overlaps', function(){
+      });
+      it('should not match a moment that does not overlap', function(){
+      });
+      it('should not match a range that does not overlap', function(){
+      });
+    });
+  });
+  describe('datemonthly', function(){
+    before(function(){
+    });
+
+    describe('parsing', function(){
+      it('should have the correct interval', function(){
+      });
+    });
+    describe('matches', function(){
+      it('should have the correct interval', function(){
+      });
+      it('should match a moment that overlaps', function(){
+      });
+      it('should match a moment range that overlaps', function(){
+      });
+      it('should not match a moment that does not overlap', function(){
+      });
+      it('should not match a range that does not overlap', function(){
+      });
+    });
+  });
+});
