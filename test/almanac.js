5a683649ca12b0261afc8c910bbde9af16881b80
Chance Dinkins <chance@cyberstride.net>
2012-09-16 14:14:34 -0400

    almost finished with single instance almanacs

--- test/almanac.js
+++ test/almanac.js
@@ -1,52 +1,97 @@
 require('should');
 var almanac = require('../')
-, moment = require('moment');
+  , moment = require('moment')
+  , Time = require('../lib/time')
+  , should = require('should');
 
 describe('Almanac', function(){
+  var schedule = {};
   describe('parsing', function(){
     describe('dates', function(){
       it('should parse dates without providing a format', function(){
-        var schedule = almanac({startDate:'10-21-1983', endDate:'10-21-2013' });
+        schedule = almanac({startDate:'10-21-1983', endDate:'10-21-2013' });
         schedule.startDate.toString().should.equal(moment(new Date(1983,9,21)).toString());
         schedule.endDate.toString().should.eql(moment(new Date(2013, 9, 21)).toString());
       });
       it('should parse dates with provided format', function(){
-        var schedule = almanac({startDate:'1983-10-21', endDate:'2013-10-21', dateFormat:'YYYY-MM-DD' });
+        schedule = almanac({startDate:'1983-10-21', endDate:'2013-10-21', format:'YYYY-MM-DD' });
         schedule.startDate.toString().should.eql(moment(new Date(1983, 9, 21)).toString());
         schedule.endDate.toString().should.equal(moment(new Date(2013, 9, 21)).toString());
       });
     });
     describe('times', function(){
       it('should get start time from start date if startTime is not provided and startDate has a time greater than 00:00:00', function(){
-        var schedule = almanac({startDate:'1983-10-21 103000', endDate:'2013-10-21', dateFormat:'YYYY-MM-DD HHmmss' });
+        schedule = almanac({startDate:'1983-10-21 103000', endDate:'2013-10-21', format:'YYYY-MM-DD HHmmss' });
         schedule.startTime.toString().should.equal('10:30:00');
       });
 
       it('should set the end time based on duration if provided', function(){
-        var schedule = almanac({startDate:'1983-10-21 083000', duration:'01:30:00', dateFormat:'YYYY-MM-DD HHmmss' });
+        schedule = almanac({startDate:'1983-10-21 083000', duration:'01:30:00', format:'YYYY-MM-DD HHmmss' });
         schedule.endTime.toString().should.equal('10:00:00');
       });
 
-      it('should get end time from end date if endTime is not provided and endDate has a time greater than 00:00:00', function(){
+      it('should get endTime from endDate if endTime is not provided and endDate has a time greater than 00:00:00', function(){
+        schedule = almanac({endDate:'2013-10-21 083000', format:'YYYY-MM-DD HHmmss' });
+        schedule.endTime.toString().should.equal('08:30:00');
       });
       it('should use the beginning of the day if startTime is not set and startDate does not have a time', function(){
+        schedule = almanac({startDate:'1983-10-21', endDate:'2013-10-21', format:'YYYY-MM-DD HHmmss' });
+        schedule.startTime.toString().should.equal('00:00:00');
       });
     });
-  });it
-
+  });
   describe('with type single', function(){
-    var schedule = {};
     before(function(){
-      schedule = almanac({type:'single'});
+      var start = moment(new Date()).add('hours', 2);
+      schedule = almanac({type:'single', startDate: start});
+      schedule.duration = '01:20:05';
     });
-    describe('next',function(){
-      it('should return null if no date is passed and current date is beyond the endDate of the event', function(){
-            //if an almanac only has type single then there is no next occurrence
-            schedule.next.should.equal(null);
+    describe('endsAt', function(){
+      it('should calculate based on duration and startsAt', function(){
+        var expectedResult = moment('09/01/2012 06:30:12').toDate();
+        schedule = almanac( { startDate:'09/01/2012 00:00:00', duration:'06:30:12', type:'single'});
+        schedule.endsAt().should.equal(expectedResult);
+      });
+    })
+    describe('calling next',function(){
+      it('should return null if a date is not a param and current date is beyond the endDate of the occasion', function(){
+        // if an almanac has type single then there is not a next occurrence beyond the startDate
+        schedule = almanac({endDate:'10-10-1983', type:'single'});
+        schedule.next(function(err, nxt){
+          should.not.exist(nxt);
+        });
+      });
+      it('should return itself if a date is not a param but current date is not beyond the startDate of the occasion', function(done){
+        schedule = almanac({startDate:'10-10-2013', type:'single', duration:'10:00:00'})
+        schedule.next(function(err, nxt){
+          nxt.should.equal(schedule);
+        });
+        done(new Error('should not make it here'));
+      });
+    });
+    describe('occursAt', function(){
+      it('should determine if the date is within the occassions occurence', function(){
+        schedule = almanac({startDate:'09/01/2012 08:00:00', duration:'06:30:00', type:'single'});
+        schedule.occursAt('09/01/2012 10:00:00').should.be.true;
+        schedule.occursAt('09/01/2013 10:00:00').should.be.false;
+      })
+    })
+    describe('occursWithin', function(){
+      describe('dates', function(){
+        it('should return true if the occassion startDate falls within a range of from and to dates', function(done){
+          schedule = almanac({startDate:'09/01/2012 08:00:00', duration:'06:30:00', type:'single'});
+          schedule.occursWithin('09/01/2012 10:00:00', '09/01/2012 12:30:00').should.be.true;
+          schedule.occursWithin('09/01/2013 10:00:00', '09/01/2013 12:30:00').should.be.false;
+          schedule.occursWithin({ from:'08/01/2012'
+                          , to: '09/02/2012'
+                          , callback: done
+                          });
+        });
       });
     });
   });
 });
+
     /*
     it('should return total requested occasions if they exist', function(){
     });
--- test/time.js
+++ test/time.js
@@ -1,3 +1,4 @@
+var should = require('should');
 var Time = require('../lib/time.js');
 describe("Time", function() {
   it('should parse a start time from a string formatted either hh:mm:ss or hhmmss', function(){
@@ -11,5 +12,15 @@ describe("Time", function() {
   it('should take a number of seconds and calculate hours, minutes, and seconds', function(){
     var t = new Time(9010);
     t.toString().should.equal('02:30:10');
+  });
+  describe('arithmetic', function(){
+    it('should add two Times to equal the result in seconds', function(){
+      var t1 = new Time("02:30:10");
+      var t2 = new Time("01:30:50");
+      var result = (t1+t2);
+      result.should.equal(14460); //4 hours, 1 minute
+      var timeResult = new Time(result);
+      timeResult.should.equal(new Time('04:01:00'))
+    });
   });
 });
